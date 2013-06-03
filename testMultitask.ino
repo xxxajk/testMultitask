e625219d6f261f33316dd3a7ff7418713f21a1e5
Andrew J. Kroll <a@oo.ms>
2013-06-03 11:23:16 -0400

    More goodness

--- testMultitask.ino
+++ testMultitask.ino
@@ -9,29 +9,40 @@
  *
  * This program tests multitasking in xmem2.
  *
+ * Note! Since the primary task (0) services serial in main(),
+ * it may look like it is not getting a fair share of CPU time. It is.
+ * Expect to see less 'L' printed! Also expect way less 'B' since that tests
+ * the Sleep() function.
+ *
  */
 
 #include <avr/pgmspace.h>
 #include <xmem.h>
 #include <avrpins.h>
 #include <stdio.h>
-static FILE mystdout;
 
 #define led 13
+
+// Uncomment to test xmem::Sleep()
+//#define TEST_TIMING
+
+static FILE mystdout;
 volatile int brightness = 128; // how bright the LED is
 volatile int fadeAmount = 8; // how many points to fade the LED by
-volatile uint32_t LEDnext_time; // fade timeout
 
 extern "C" unsigned int freeHeap();
 
 // Fun! Serial stuff is thread safe!
+
 static int my_putc(char c, FILE *t) {
         Serial.write(c);
 }
 
 void loop() {
         // Look mom! No hands!
+#ifndef TEST_TIMING
         printf("L");
+#endif
 }
 
 
@@ -39,35 +50,50 @@ void loop() {
 
 void task_A(void) {
         for (;;) {
+#ifndef TEST_TIMING
                 printf("A");
+#endif
         }
 }
 
-void breath(void) {
+void task_B(void) {
         for (;;) {
+#ifdef TEST_TIMING
+                uint32_t x = millis();
+                while (x == millis());
+                x = millis();
+                xmem::Sleep(30);
+                uint32_t y = millis();
+                printf("%i\r\n", y - x);
+#else
                 printf("B");
-                if (millis() >= LEDnext_time) {
-                        LEDnext_time = millis() + 30;
+                xmem::Sleep(30);
+#endif
+                // set the brightness of pin 9:
+                analogWrite(led, brightness);
 
-                        // set the brightness of pin 9:
-                        analogWrite(led, brightness);
+                // change the brightness for next time through the loop:
+                brightness = brightness + fadeAmount;
 
-                        // change the brightness for next time through the loop:
-                        brightness = brightness + fadeAmount;
-
-                        // reverse the direction of the fading at the ends of the fade:
-                        if (brightness <= 0) {
-                                brightness = 0;
-                                fadeAmount = -fadeAmount;
-                        }
-                        if (brightness >= 255) {
-                                brightness = 255;
-                                fadeAmount = -fadeAmount;
-                        }
+                // reverse the direction of the fading at the ends of the fade:
+                if (brightness <= 0) {
+                        brightness = 0;
+                        fadeAmount = -fadeAmount;
+                }
+                if (brightness >= 255) {
+                        brightness = 255;
+                        fadeAmount = -fadeAmount;
                 }
         }
 }
 
+void task_C(void) {
+        for (;;) {
+#ifndef TEST_TIMING
+                printf("C");
+#endif
+        }
+}
 void setup() {
         // declare pin 9 to be an output:
         pinMode(led, OUTPUT);
@@ -81,14 +107,14 @@ void setup() {
         Serial.begin(115200);
 
         // Note! tasks do NOT actually start here. They start after the return to main()!
+        // The values t[1-3] are thrown out. You can save them to a global if you need to.
 
-        uint8_t t1 = xmem::SetupTask(breath);
+        uint8_t t1 = xmem::SetupTask(task_A);
         xmem::StartTask(t1);
-        uint8_t t2 = xmem::SetupTask(task_A);
+        uint8_t t2 = xmem::SetupTask(task_B);
         xmem::StartTask(t2);
-        LEDnext_time = millis() + 1;
-
-
+        uint8_t t3 = xmem::SetupTask(task_C);
+        xmem::StartTask(t3);
 }
 
 
